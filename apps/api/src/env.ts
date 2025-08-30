297c4e9ff84a8313d300772f77779784564695f9
ilmari-h <52321471+ilmari-h@users.noreply.github.com>
2025-08-30 15:51:43 +0200

    Fix PORT var validation

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -4,7 +4,7 @@ const envSchema = z.object({
   JWT_SECRET: z.string(),
   DATABASE_URL: z.string(),
   REDIS_CONNECTION_STRING: z.string(),
-  PORT: z.number().default(3001),
+  PORT: z.string().or(z.number()).default("3001"),
   GITHUB_CLIENT_ID: z.string(),
   GITHUB_CLIENT_SECRET: z.string(),
   GITHUB_CALLBACK_URL: z.string(),
