95bf045d1d91b09bc1847ea9f221d4ff808cac81
ilmari-h <52321471+ilmari-h@users.noreply.github.com>
2025-09-15 13:44:08 +0200

    Adjust conversion tracking

--- apps/web/app/tracker.ts
+++ apps/web/app/tracker.ts
@@ -8,8 +8,9 @@ export const recordConversion = (value: number) => {
   if((window as any).gtag) {
     (window as any).gtag('event', "conversion", {
       'send_to': 'AW-16519815193/-I84COnLkpsbEJnAocU9',
-      'value': value,
-      'currency': 'USD'
+      'value': value ? value : 1, // Some value is required
+      'currency': 'USD',
+      'payment_type': value ? 'stripe' : 'free',
     });
   }
 
