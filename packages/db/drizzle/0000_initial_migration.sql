a4c6341dec430db8be4e928f556803f580da5750
ilmari-h <52321471+ilmari-h@users.noreply.github.com>
2025-10-05 16:30:18 +0200

    Regenerate initial migration file

--- packages/db/drizzle/0000_initial_migration.sql
+++ packages/db/drizzle/0000_initial_migration.sql
@@ -5,6 +5,7 @@ CREATE TABLE "users" (
 	"max_organizations" integer DEFAULT 1 NOT NULL,
 	"max_users_per_organization" integer DEFAULT 1 NOT NULL,
 	"email" text,
+	"stripe_customer_id" text,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL,
 	CONSTRAINT "users_name_unique" UNIQUE("name")
@@ -12,7 +13,7 @@ CREATE TABLE "users" (
 --> statement-breakpoint
 CREATE TABLE "organizations" (
 	"id" text PRIMARY KEY NOT NULL,
-	"name" text DEFAULT '8x8uq5zsj28i3hz9' NOT NULL,
+	"name" text NOT NULL,
 	"description" text,
 	"created_by_id" text NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
@@ -56,9 +57,8 @@ CREATE TABLE "projects" (
 CREATE TABLE "environments" (
 	"id" text PRIMARY KEY NOT NULL,
 	"name" text NOT NULL,
-	"free_form" boolean DEFAULT false NOT NULL,
-	"project_id" text NOT NULL,
-	"preserved_versions" integer DEFAULT 100 NOT NULL,
+	"project_id" text,
+	"organization_id" text,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL
 );
@@ -129,6 +129,21 @@ CREATE TABLE "user_public_keys" (
 	CONSTRAINT "user_public_key_unique" UNIQUE("user_id","public_key_id")
 );
 --> statement-breakpoint
+CREATE TABLE "variable_groups" (
+	"id" text PRIMARY KEY NOT NULL,
+	"environment_id" text NOT NULL,
+	"description" text,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "environment_variable_groups" (
+	"required_by_environment_version_id" text NOT NULL,
+	"variable_group_id" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
 ALTER TABLE "organizations" ADD CONSTRAINT "organizations_created_by_id_users_id_fk" FOREIGN KEY ("created_by_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "organization_roles" ADD CONSTRAINT "organization_roles_organization_id_organizations_id_fk" FOREIGN KEY ("organization_id") REFERENCES "public"."organizations"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "organization_roles" ADD CONSTRAINT "organization_roles_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
@@ -136,6 +151,7 @@ ALTER TABLE "organization_invites" ADD CONSTRAINT "organization_invites_organiza
 ALTER TABLE "organization_invites" ADD CONSTRAINT "organization_invites_created_by_users_id_fk" FOREIGN KEY ("created_by") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "projects" ADD CONSTRAINT "projects_organization_id_organizations_id_fk" FOREIGN KEY ("organization_id") REFERENCES "public"."organizations"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "environments" ADD CONSTRAINT "environments_project_id_projects_id_fk" FOREIGN KEY ("project_id") REFERENCES "public"."projects"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "environments" ADD CONSTRAINT "environments_organization_id_organizations_id_fk" FOREIGN KEY ("organization_id") REFERENCES "public"."organizations"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "environment_access" ADD CONSTRAINT "environment_access_environment_id_environments_id_fk" FOREIGN KEY ("environment_id") REFERENCES "public"."environments"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "environment_access" ADD CONSTRAINT "environment_access_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "environment_access" ADD CONSTRAINT "environment_access_access_token_id_access_tokens_id_fk" FOREIGN KEY ("access_token_id") REFERENCES "public"."access_tokens"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
@@ -148,6 +164,9 @@ ALTER TABLE "access_tokens" ADD CONSTRAINT "access_tokens_created_by_users_id_fk
 ALTER TABLE "access_tokens" ADD CONSTRAINT "access_tokens_public_key_id_public_keys_id_fk" FOREIGN KEY ("public_key_id") REFERENCES "public"."public_keys"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "user_public_keys" ADD CONSTRAINT "user_public_keys_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "user_public_keys" ADD CONSTRAINT "user_public_keys_public_key_id_public_keys_id_fk" FOREIGN KEY ("public_key_id") REFERENCES "public"."public_keys"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "variable_groups" ADD CONSTRAINT "variable_groups_environment_id_environments_id_fk" FOREIGN KEY ("environment_id") REFERENCES "public"."environments"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "environment_variable_groups" ADD CONSTRAINT "environment_variable_groups_required_by_environment_version_id_environment_versions_id_fk" FOREIGN KEY ("required_by_environment_version_id") REFERENCES "public"."environment_versions"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "environment_variable_groups" ADD CONSTRAINT "environment_variable_groups_variable_group_id_variable_groups_id_fk" FOREIGN KEY ("variable_group_id") REFERENCES "public"."variable_groups"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 CREATE UNIQUE INDEX "unique_environment_access_user" ON "environment_access" USING btree ("environment_id","user_id");--> statement-breakpoint
 CREATE UNIQUE INDEX "unique_environment_access_token" ON "environment_access" USING btree ("environment_id","access_token_id");--> statement-breakpoint
 CREATE INDEX "access_token_value_idx" ON "access_tokens" USING btree ("value");--> statement-breakpoint
